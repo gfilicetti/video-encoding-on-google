041cb919792f34086ca8b080d7c1edf026ba6df5
Michael Bychkowski <mbychkowski@google.com>
2025-03-06 23:26:15 -0500

    Create event architecture for encoder

--- scripts/04-setup-iam.sh
+++ scripts/04-setup-iam.sh
@@ -20,16 +20,13 @@ source .env
 # Add roles to Github Actions Google Cloud Service Account.
 for SUCCINCT_ROLE in \
     artifactregistry.admin \
+    artifactregistry.admin \
     cloudbuild.connectionAdmin \
     cloudbuild.builds.editor \
-<<<<<<< HEAD
     clouddeploy.developer \
     clouddeploy.operator \
     compute.networkAdmin \
     container.clusterAdmin \
-=======
-    roles/compute.viewer \
->>>>>>> 20ba65f (update IAM roles on compute engine default sa)
     iam.serviceAccountAdmin \
     iam.serviceAccountUser \
     logging.configWriter \
