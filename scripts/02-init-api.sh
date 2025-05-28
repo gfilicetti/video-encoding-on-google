e2618b2a2b7f474bacf34816ad82e093c5893aa2
Adrian Graham <adriangraham@google.com>
2025-05-27 17:04:50 -0700

    updating boilerplate

--- scripts/02-init-api.sh
+++ scripts/02-init-api.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,6 +35,7 @@ for GOOGLE_CLOUD_API in \
   storage.googleapis.com \
   workflows.googleapis.com \
     ; do
-  gcloud services enable --project ${GCP_PROJECT_ID} \
+  gcloud services enable \
+    --project ${GCP_PROJECT_ID} \
     ${GOOGLE_CLOUD_API}
 done
